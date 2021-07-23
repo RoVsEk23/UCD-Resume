d2e1aab22ebc8ea850ab9bdb84765ff259aaf313
RoVsEk23 <rohitk23@gmail.com>
2021-07-23 09:48:44 +0000

    map coordinates changed

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 8,
     center: {
-      lat: 46.619261,
-      lng: -33.134766,
+      lat: 51.096345,
+      lng: -0.210857,
     },
   });
   var labels = "ABCDEFGHIJKLMNOOPQRSTUVWXYZ";
